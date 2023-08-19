07650ce8f0ba40755ddc92863f914315074b572f
Giancarlo <giancarlo2709@gmail.com>
2023-08-18 23:22:22 -0500

    adding app dragon ball z

---
+++ 02-bases/src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+import { v4 as uuid } from 'uuid';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent {
+
+  @Output()
+  public onDeleteId: EventEmitter<string> = new EventEmitter<string>();
+
+  @Input()
+  public characterList: Character[] = [{
+    id: uuid(),
+    name: 'Trunks',
+    power: 10
+  }];
+
+  emitDeleteCharacter(id?: string): void {
+    if ( !id ) return;
+    this.onDeleteId.emit(id);
+  }
+
+}
